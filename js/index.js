26889761ee4a63afd10d483ceafcc78a5e528422
xc99234567 <xc99234567@gmail.com>
2021-10-31 20:25:56 +0800

    新增首頁輪播圖

---
+++ js/index.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+    // 輪播套件
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 10000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+
+}
