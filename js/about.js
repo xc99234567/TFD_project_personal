f26049313575216a56ca1c7f7885ee29beb271e7
xc99234567 <xc99234567@gmail.com>
2021-11-12 00:10:46 +0800

    關於我完成RWD

--- js/about.js
+++ js/about.js
@@ -1,10 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function(){
 
-    // 表單正規
-    const text_ele = document.querySelectorAll("form .text_ele");
-    const inputs = document.querySelectorAll("form .text_ele input");
-    console.log(text_ele);
-    console.log(inputs);
+    AOS.init();
 
-    
 });
